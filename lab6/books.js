a1da86b37bb8f9b00d71fbabb6a3059438a7c36a
Алиса Дьячкова <sparrowm24@mail.ru>
2025-05-15 21:00:54 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон Дао и дэ',
+        publicationYear: 2022,
+        price: 430,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 300,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 450,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 350,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 600,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 800,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Дэниел Киз',
+        name: 'Цветок для Элджернона',
+        publicationYear: 1966,
+        price: 400,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Габриэль Гарсиа Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 1967,
+        price: 550,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джек Керуак',
+        name: 'На дороге',
+        publicationYear: 1957,
+        price: 300,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Марио Пьюзо',
+        name: 'Крестный отец',
+        publicationYear: 1969,
+        price: 450,
+        bestseller: true,
+        inStock: true
+    }
+];
